cf70f79d7d5b8a8b39d420714b90c1b2a4bfa734
sintaro-katuta <sintaro.katuta@gmail.com>
2024-11-07 14:25:20 +0900

    [chore] 29 余計な部分の削除

---
+++ changelog.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  disableEmoji: false,
+  format: "[{type}] {subject}",
+  list: [
+    "fix",
+    "add",
+    "update",
+    "remove",
+    "test",
+    "style",
+    "chore",
+    "docs",
+    "perf",
+    "ci",
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: ['type', 'subject', 'body', 'breaking', 'issues', 'lerna'],
+  scopes: [],
+  types: {
+    chore: {
+      description: "ドキュメントの生成やビルドプロセス、ライブラリなどの変更",
+      value: "chore",
+    },
+    ci: {
+      description: "CI用の設定やスクリプトに関する変更",
+      value: "ci",
+    },
+    docs: {
+      description: "ドキュメントのみの変更",
+      value: "docs",
+    },
+    add: {
+      description: "新機能",
+      value: "add",
+    },
+    fix: {
+      description: "不具合の修正",
+      value: "fix",
+    },
+    perf: {
+      description: "パフォーマンス改善を行うためのコードの変更",
+      value: "perf",
+    },
+    update: {
+      description: "バグ修正や機能の追加を行わないコードの変更",
+      value: "update",
+    },
+    remove: {
+      description: "ファイルの削除",
+      value: "remove",
+    },
+    style: {
+      description: "コードの処理に影響しない変更（スペースや書式設定など）",
+      value: "style",
+    },
+    test: {
+      description: "テストコードの変更",
+      value: "test",
+    },
+  },
+};
