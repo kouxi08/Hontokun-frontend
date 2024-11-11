27cf80d37fd52020b85001f5ffdb3326da676e2e
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-11 11:33:21 +0900

    【add】refs 12 headerの作成

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="bg-primary text-black shadow-md font-zenMaru rounded-b-lg h-22 w-full"> <!-- 高さを88pxに設定 -->
+      <div class="container mx-auto pb-2 flex justify-between items-center h-full px-6"> <!-- ヘッダー内の要素を高さに合わせて調整 -->
+        <div class="flex space-x-2 mt-12"> <!-- 56pxのマージン -->
+          <h1 class="text-sm mt-2 font-bold">ニュース探偵</h1>
+          <span class="text-2xl font-bold">ホントくん</span>
+        </div>
+        <nav class="mt-10"> 
+          <a href="#" class="bg-white rounded-lg h-8 w-24 flex items-center justify-center" role="button">
+            ログイン
+          </a>
+        </nav>
+      </div>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppHeader',
+  };
+  </script>
+  
+  <style scoped>
+  /* カスタムスタイルが必要なら追加 */
+  </style>
+  
