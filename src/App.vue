69626eec8a7d6322810054da0b8aa19f2746b974
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-11 16:07:40 +0900

    [add] refs 13 フッターの作成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <Header></Header>
+  <Footer></Footer>
 </template>
 
 <script setup>
 //import HelloWorld from './components/HelloWorld.vue'
 import Header from './components/HeaderComponent.vue';
-
+import Footer from './components/FooterComponent.vue';
 </script>
 
 <style>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer class="bg-white  flex flex-col  items-center">
+    <span class="text-black mb-6">ページのトップに戻る</span> <!-- 24pxのスペース -->
+    <span class="text-gray-600">Copyright© 1000yen All Rights Reserved.</span>
+  </footer>
+</template>ß
