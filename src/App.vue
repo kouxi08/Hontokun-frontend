2ca9fb51ac922c574b0b188d8dff0ea72734d22b
sintaro-katuta <sintaro.katuta@gmail.com>
2024-11-12 16:24:46 +0900

    [add] 10 ボタンコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Button>
+    ログイン
+  </Button>
+
+  <Button color="primary">
+    ログイン
+  </Button>
+
+  <Button color="secondary">
+    ログイン
+  </Button>
+
+  <Button color="accent">
+    ログイン
+  </Button>
+
+  <Button color="danger">
+    ログイン
+  </Button>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Button from './components/ButtonComponent.vue'
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: avenir, helvetica, arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
