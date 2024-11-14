4da7e6b78747dde235cfe515e1ea5e5b5efa0270
sintaro-katuta <sintaro.katuta@gmail.com>
2024-11-14 19:28:19 +0900

    [add] refs 20 経験値コンポーネントの作成

---
+++ src/components/XpComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex w-[152px] h-[64px] flex-wrap gap-[8px]">
+    <img v-for="index in 10" :key="index" :src="index <= props.value ? '/icons/star-true.svg' : '/icons/star-false.svg'"
+      :alt="`Star ${index}`">
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0
+  }
+});
+</script>
