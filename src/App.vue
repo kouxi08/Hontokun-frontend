63e4e6052e014aa600a97a0030768ea4d88326d7
futo-nakagawa <2210245@ecc.ac.jp>
2024-11-15 15:31:25 +0900

    [update] refs #21 メッセージコンポーネントの更新

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
+  <Message>
+    ようこそ!<br>
+    $user_name探偵事務所へ<br>
+    僕は助手のホントくん<br>
+    よろしくね！
+  </Message>
 </template>
 
 <script setup>
-
+import Header from './components/HeaderComponent.vue';
+import Footer from './components/FooterComponent.vue';
+import Message from './components/MessageComponent.vue';
 </script>
 
 <style>
 #app {
-  font-family: avenir, helvetica, arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/MessageComponent.vue
+++ src/components/MessageComponent.vue
@@ -1,38 +1,31 @@
 <template>
-  <div class="mx-6">
+  <div class="px-6 h-screen flex flex-col">
     <div :class="positionClasses">
-      <slot />
+      <slot /> 
     </div>
   </div>
 </template>
-  
-  <script setup>
-  import { computed } from 'vue';
-  const props = defineProps({
-    position: {
-      type: String,
-      default: 'bottom', // 'top', 'middle', or 'bottom'
-      validator: (value) => ['top', 'middle', 'bottom'].includes(value)
-    }
-  });
-  
-  const positionClasses = computed(() => {
-    const baseClasses = "w-full h-32 border-[3px] border-primary rounded-[24px] shadow-[4px_4px_2px_0_rgba(0,0,0,0.4)] flex justify-center items-center";
-  
-    const positionStyles = {
-      top: 'absolute top-[172px]',
-      middle: 'absolute top-[calc(50%-64px)]',
-      bottom: 'absolute bottom-[172px]'
-    };
-  
-    return `${baseClasses} ${positionStyles[props.position] || ''}`;
-  });
-  </script>
-  
-  <style scoped>
-  .mx-6 {
-    position: relative;
-    height: 852px; /* Container height to enable proper positioning */
-  }
-  </style>
-  
+      
+      <script setup>
+      import { computed } from 'vue';
+      
+      const props = defineProps({
+        position: {
+          type: String,
+          default: 'bottom', // 'top', 'middle', or 'bottom'
+          validator: (value) => ['top', 'middle', 'bottom'].includes(value)
+        }
+      });
+      
+      const positionClasses = computed(() => {
+        const baseClasses = "w-full h-32 px-8 border-[3px] border-primary rounded-[24px] shadow-[4px_4px_2px_0_rgba(0,0,0,0.4)] flex justify-start items-center text-left";
+      
+        const positionStyles = {
+          top: 'mt-[50%]',
+          middle: 'mt-[100%]', 
+          bottom: 'mt-auto mb-[50%]' 
+        };
+      
+        return `${baseClasses} ${positionStyles[props.position] || ''}`;
+      });
+      </script>
