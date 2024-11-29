420eeb663a6da5116d5ce60dc6c612b7e0a5d174
sintaro-katuta <sintaro.katuta@gmail.com>
2024-11-29 16:03:14 +0900

    [add] refs 33 フッターの文字色の変更

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,7 +4,7 @@
       <span class="text-black font-zenMaru" @click="pageTop">ページのトップに戻る</span>
       <Icon name="arrow-up" width=8 height=8 />
     </div>
-    <span class="text-gray-600 text-[10px] pb-[24px]">Copyright© 1000yen All Rights Reserved.</span>
+    <span class="text-[#999999] text-[10px] pb-[24px]">Copyright© 1000yen All Rights Reserved.</span>
   </footer>
 </template>
 
