518b0b6237756c66e00d98a54b7005cea5c30f5e
futo-nakagawa <2210245@ecc.ac.jp>
2024-12-02 14:36:29 +0900

    [add] refs #28 カードコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
-<template>  
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-50">
+    <CardComponent
+      icon-name="風呂敷ネコ"
+      icon-image="/furoshiki.png"
+      :accuracy="40"
+      :attempts="attemptData"
+    />
+  </div>
 </template>
 
 <script setup>
+import CardComponent from './components/CardComponent.vue';
+
+// 渡すデータ
+const attemptData = [
+  { timeAgo: "3分前", accuracy: 85 },
+  { timeAgo: "2時間前", accuracy: 42 },
+  { timeAgo: "3時間前", accuracy: 50 },
+];
 </script>
