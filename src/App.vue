bb1d0d21d51bc741818c3fbd024e2582f764f978
futo-nakagawa <2210245@ecc.ac.jp>
2024-12-10 15:42:55 +0900

    [add] refs 63 解説コンポーネント完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <RouterView />
+  <News question="これはフェイクニュース？" title="台風15号接近 首都圏厳戒態勢" content="台風15号が関東地方に接近中。気象庁は警戒を呼びかけ、各地で厳重な備えが進む。東京都は午後から公共交通機関の計画運休を発表。スーパーには買い出しの長蛇の列。企業は在宅勤務を推奨し、学校は休校を決定。避難所も開設され始めた。強風と豪雨に備え、住民の緊張が高まる。明日未明に最接近の見込み。" />
+  <Explain content="「台風15号」など台風番号で調べることでより検索しやすくなります。「関東」「接近」で関東に接近した台風の情報が検索で出てきやすくなります。" hintword="台風15号 関東 接近" />
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router';
+import News from "./components/NewsComponent.vue"
+import Explain from "./components/ExplainComponent.vue"
 </script>
---
+++ src/components/ExplainComponent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="font-zenMaru mx-[48px] p-[16px] shadow-lg rounded-[6px] flex flex-col">
+    <!-- ヘッダー部分 -->
+    <div class="w-full h-[40px] grid grid-cols-3 justify-between items-center top-4">
+      <Icon  
+        name="card-left" 
+        width="24" 
+        height="24" 
+      />
+      <p class="font-bold text-[16px] text-center">解説</p>
+      <Button size="xs" color="danger" class="justify-self-end">報告</Button>
+    </div>
+
+    <!-- 正解ボックス -->
+    <div class="w-full grid grid-cols-3 justify-between items-center pt-[24px]">
+      <p class="justify-self-start font-bold text-white text-[16px] bg-accent py-[4px] px-[8px] rounded-[8px]">
+        正解
+      </p>
+      <Icon 
+        class="justify-self-center"
+        name="correct" 
+        width="24" 
+        height="24" 
+      />
+      <a class="justify-self-end text-blue-500 text-[12px] font-bold" href="example.com">
+        ニュース記事へ
+      </a>
+    </div>
+    
+
+    <!-- 検索ワード表示 -->
+    <div class="mt-[32px] flex items-center justify-between border rounded-full p-[8px] shadow">
+      <p class="text-black font-bold text-[12px] px-[8px] flex-1">
+        {{ hintword }}
+      </p>
+      <button 
+        class="bg-primary text-black font-bold text-[14px] py-[6px] px-[16px] rounded-full shadow"
+        @click="searchGoogle"
+      >
+        検索
+      </button>
+    </div>
+
+    <!-- 説明テキスト -->
+    <p class="mt-[32px] font-bold text-[12px] tracking-wider leading-[32px]">
+      {{ props.content }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import Button from './ButtonComponent.vue';
+import Icon from './IconComponent.vue';
+
+const props = defineProps({
+  content: {
+    type: String,
+    default: ""
+  },
+  hintword: {
+    type: String,
+    default: ''
+  }
+});
+
+function searchGoogle() {
+  const query = encodeURIComponent(props.hintword); // URLエンコード
+  const googleSearchUrl = `https://www.google.com/search?q=${query}`;
+  window.open(googleSearchUrl, "_blank"); // 新しいタブで検索ページを開く
+}
+</script>
