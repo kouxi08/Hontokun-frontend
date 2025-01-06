e00431905c00c6a0f192e7a8203160e8a37b8115
sintaro-katuta <sintaro.katuta@gmail.com>
2025-01-06 14:18:23 +0900

    [add] refs 78 ログインしていたらトップ画面でメイン画面に移動する

--- src/components/page/topPage.vue
+++ src/components/page/topPage.vue
@@ -12,14 +12,16 @@
       今すぐ挑戦
     </Button>
     <div class="py-[104px]">
-      <p class="text-center text-white stroke-accent-4 font-zenMaru text-[32px] font-black mx-[24px] my-[56px] leading-[48px]">
+      <p
+        class="text-center text-white stroke-accent-4 font-zenMaru text-[32px] font-black mx-[24px] my-[56px] leading-[48px]">
         フェイクニュースに<br />騙されていませんか？</p>
       <p class="text-[18px] font-zenMaru font-black text-center leading-[48px] mb-[72px]">
         日本での調査では
         <span class="bg-accent rounded-[8px] p-[4px] mx-[4px] text-white stroke-black-2">４人中３人</span>は、<br />
         騙されている可能性があり<br />
         その中の半分の人は偽の情報を知らぬ間に<br />
-        拡散してしまっています。</p>
+        拡散してしまっています。
+      </p>
 
       <p class="text-[18px] font-zenMaru font-black text-center leading-[48px]">
         フェイクニュースは、
@@ -35,31 +37,18 @@
     </div>
     <div class="bg-primary w-fit mx-auto rounded-[12px] my-[56px]">
       <p
-        class="font-zenMaru font-black  text-[20px] text-center text-white stroke-braun py-[8px] px-[16px] stroke-braun-4"
-      >
+        class="font-zenMaru font-black  text-[20px] text-center text-white stroke-braun py-[8px] px-[16px] stroke-braun-4">
         キャラクターデザイン
       </p>
     </div>
     <div class="mb-[240px]">
       <p class="font-black font-zenMaru text-[20px] text-center my-[16px]">{{ cats[index].name }}</p>
       <div class="flex transition duration-150 ease-in-out">
-        <img 
-          :src="cats[previousCat(index)].img" 
-          :alt="cats[previousCat(index)].name"
-          class="w-[64px] object-contain animate-pulse cursor-pointer" 
-          @click="index = previousCat(index)" 
-        />
-        <img 
-          :src="cats[index].img" 
-          :alt="cats[index].img" 
-          class="mx-auto" 
-        />
-        <img 
-          :src="cats[nextCat(index)].img" 
-          :alt="cats[nextCat(index)].name"
-          class="w-[64px] object-contain animate-pulse cursor-pointer" 
-          @click="index = nextCat(index)"
-        />
+        <img :src="cats[previousCat(index)].img" :alt="cats[previousCat(index)].name"
+          class="w-[64px] object-contain animate-pulse cursor-pointer" @click="index = previousCat(index)" />
+        <img :src="cats[index].img" :alt="cats[index].img" class="mx-auto" />
+        <img :src="cats[nextCat(index)].img" :alt="cats[nextCat(index)].name"
+          class="w-[64px] object-contain animate-pulse cursor-pointer" @click="index = nextCat(index)" />
       </div>
       <p class="font-zenMaru text-[14px] font-bold text-center m-[24px] leading-[32px]">{{ cats[index].description }}
       </p>
@@ -73,6 +62,17 @@ import Header from '../HeaderComponent.vue'
 import Footer from '../FooterComponent.vue'
 import Button from '../ButtonComponent.vue'
 import { ref } from 'vue'
+import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const router = useRouter()
+const auth = getAuth()
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    router.push({ name: "mainPage" });
+  }
+})
 
 const cats = [
   { name: "ホントくん", img: "/hontokun.png", description: "我らが主人公ホントくん！今まで難解な事件を解決してきた敏腕探偵ねこ。魚を盗む元泥棒ねこ。" },
