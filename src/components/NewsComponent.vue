2866c760a8b628b3008c02956c9293a9cd4fea0d
sintaro-katuta <sintaro.katuta@gmail.com>
2025-01-07 14:13:05 +0900

    [fix] refs 38 クラス名削除

--- src/components/NewsComponent.vue
+++ src/components/NewsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hgioejlvlbrukb w-full h-fit font-zenMaru flex flex-col">
+  <div class="w-full h-fit font-zenMaru flex flex-col">
     <div class="mx-[48px] p-[16px] h-[408px] bg-white shadow-lg rounded-[6px] flex flex-col gap-[16px] z-10">
       <div class="relative">
         <img :src="props.img" alt="" class="w-full h-[104px] rounded-[6px] object-cover">
