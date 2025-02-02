cbce3406a796883cb69b7a7b94aab3a5102f71dc
sintaro-katuta <sintaro.katuta@gmail.com>
2025-02-03 02:56:55 +0900

    [fix] refs 140 メッセージコンポーネントが切り替わるように修正
    ✅ Closes: #140

--- src/components/modules/MessageComponent.vue
+++ src/components/modules/MessageComponent.vue
@@ -2,6 +2,26 @@
   <div
     class="mx-[32px] border-[3px] border-primary rounded-[24px] leading-[1.7rem] shadow-button
       flex justify-center p-[16px] items-center text-center bg-white font-zenMaru font-bold whitespace-pre-wrap animate-fadeIn">
-    <slot></slot>
+    {{ messages[currentMessage] }}
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue"
+const props = defineProps({
+  messages: {
+    type: Array,
+    required: true,
+  },
+});
+
+const currentMessage = ref(0)
+
+setTimeout(() => {
+  if (currentMessage.value !== props.messages.length - 1) {
+    currentMessage.value++
+  }
+}, 3000);
+
+
+</script>
--- src/components/page/mainPage.vue
+++ src/components/page/mainPage.vue
@@ -12,9 +12,7 @@
         </div>
       </div>
       <div>
-        <Message class="mt-[5%]">
-          {{ messages[0] }}
-        </Message>
+        <Message class="mt-[5%]" :messages="messages" />
         <img :src="costume" alt="" class="mx-auto my-[16px] xs:w-[128px] md:w-[192px]">
         <button
           class="w-[136px] h-[136px] bg-[#FF6633] rounded-full text-white text-[32px] border-4 border-white flex items-center justify-center font-black font-zenMaru shadow-[0_0_4px_0_rgba(171,171,171,0.25)] mx-auto mt-[120px] hover:translate-y-[2px] md:mt-[32px] lg:mt-[40px]"
@@ -42,14 +40,14 @@ const costume = ref('')
 const loading = ref(false)
 const messages = ref([])
 
-onMounted(async() => {
+onMounted(async () => {
   try {
     const main = await AxiosInstance.get('/main')
     user.value = main.data.user
     costume.value = main.data.costume.url
     loading.value = true
     messages.value = [
-      `ようこそ！\n${user.value.nickname}探偵事務所へ\n僕は助手のホントくん\nよろしくね！`
+      `ようこそ！\n${user.value.nickname}探偵事務所へ\n僕は助手のホントくん\nよろしくね！`,
     ];
   } catch (error) {
     console.error('データの取得に失敗しました:', error)
